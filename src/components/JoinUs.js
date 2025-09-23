68355a3f3fed7418a9a23f3312d178bf6998c3d8
Satyajit Roy <thecgectimesofficial@gmail.com>
2025-09-23 23:01:51 +0530

    Added

--- src/components/JoinUs.js
+++ src/components/JoinUs.js
@@ -6,42 +6,8 @@ const JoinUs = () => (
     <Container>
       <Row className="align-items-center text-center text-md-start">
         
-        <Col md={4} className="mb-4 mb-md-0">
-          <h2 className="fw-bold mb-3">
-            Ready to <span className="text-warning">Join Entrepreneurship Challenge 2025?</span>
-          </h2>
-          <p className="lead mb-4">
-            🚀 Got ideas? Let’s solve big problems together!
-          </p>
-          <Button
-            variant="warning"
-            size="lg"
-            href="https://docs.google.com/forms/d/e/1FAIpQLSdsepz_32xhRAHm7178TTUDOSBsrErk1D7BwLmdGhUDq8Vt0g/viewform?pli=1" 
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Join Now
-          </Button>
-        </Col>
-
-        <Col md={4} className="mb-4 mb-md-0 text-center">
-          <h2 className="fw-bold mb-3">
-            <span className="text-info">SOCIAL MEDIA EVENT?</span>
-          </h2>
-          <p className="lead mb-4">
-            Who knew 3 random words could make you the next Elon Musk? 🤯✨ 
-          </p>
-          <Button
-            variant="info"
-            size="lg"
-            href="https://qr.me-qr.com/PiQ5rXDQ"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Collaborate
-          </Button>
-        </Col>
-
+        
+            
         <Col md={4}>
           <h2 className="fw-bold mb-3">
             Want to <span className="text-danger">Join E-Cell?</span>
