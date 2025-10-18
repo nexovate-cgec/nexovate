fbfd3b3670e6e969494f2d282fae267e8bda33f5
Satyajit Roy <thecgectimesofficial@gmail.com>
2025-10-18 21:35:58 +0530

    Change in year in testimonial

--- src/data/testimonial.js
+++ src/data/testimonial.js
@@ -9,37 +9,37 @@ export const testimonialsData = [
   {
     quote: "Being a part of CGEC ECELL helped me turn my ideas into reality. The support and environment were amazing.",
     name: "Nabajyoti Nag",
-    position: "Batch of 2023",
+    position: "Batch of 2027",
     img: member1,
   },
   {
     quote: "E-Cell's sessions and interactive workshops helped me gain practical insights into entrepreneurship and innovation. I am grateful for the platform, especially because Eureka 2025 gave me a chance to showcase my ideas to a wide audience.",
     name: "Shubham Mondal",
-    position: "Batch of 2023",
+    position: "Batch of 2027",
     img: member2,
   },
   {
     quote: "Joining E-Cell was one of the best decisions in my college life. The supportive atmosphere and engaging activities helped me develop valuable skills and broaden my perspective on innovation. I truly appreciate how E-Cell motivated me to think differently and take initiative for my own growth.",
     name: "Shirsak Roy",
-    position: "Batch of 2023",
+    position: "Batch of 2027",
     img: member3,
   },
   {
     quote: "Being part of E-Cell inspired me to push my creative boundaries and explore new opportunities in entrepreneurship. The Social Media Challenge was a wonderful platform to share my ideas and connect with other motivated students. It boosted my confidence and made my experience with E-Cell truly memorable",
     name: "Joydeep Mitra",
-    position: "Batch of 2023",
+    position: "Batch of 2027",
     img: member4,
   },
   {
     quote: "E-Cell not only connected me to the right people through its networking events but also gave me the courage to think bold. The experience reached another level when I got the chance to participate in Eureka 2025, which was an amazing milestone.",
     name: "Dibyendu Koley",
-    position: "Batch of 2023",
+    position: "Batch of 2027",
     img: member5,
   },
   {
     quote: "Being a part of E-Cell has been a transformative experience. The events and mentorship programs provided me with practical knowledge and inspired me to pursue my entrepreneurial goals with confidence. E-Cell truly fosters a community where ideas can grow and thrive",
     name: "Bhargabi Mukherjee",
-    position: "Batch of 2024",
+    position: "Batch of 2028",
     img: member6,
   },
 ];
